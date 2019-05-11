8ba2a39daf4760fd868b7ec3b6681a22b4ff229a
Jason Perelshteyn <j.b.shteyn@gmail.com>
2019-05-11 16:10:48 -0400

    update context buffer

--- matchseq.py
+++ matchseq.py
@@ -19,29 +19,56 @@ class BufferList:
         if self.curr_size == 1:
             self.end = node
             self.front = node
-            return        
+            return None   
         self.end.next = node
         self.end = node
         if self.curr_size > self.size:
+            ret = self.front.val
             self.front.next.prev = None
             self.front = self.front.next
             self.curr_size -= 1
+            return ret
+        return None
+
+    @property
+    def is_full(self):
+        return self.curr_size == self.size
 
     def reader(self):
         node = self.front
         while node:
             yield node.val
             node = node.next
-        
+
 
 class ContextBuffer:
-    def __init__(self, string_reader, x, y):
+    def __init__(self, string_reader, front_len, data_len, back_len):
         self.string_reader = string_reader
-        self.x = x
-        self.y = y
-        
-    def __next__(self)
-        
+        self.front = BufferList(front_len)
+        self.data = BufferList(data_len)
+        self.back = BufferList(back_len)
+        while not self.back.is_full:
+            c = next(self.string_reader)
+            val = self.back.add(c)
+            
+    def __iter__(self):
+        return self
+                
+    def next(self):
+        c = next(self.string_reader)
+        curr_val = self.back.add(c)
+        prev_val = self.data.add(curr_val)
+        if prev_val:
+            self.front.add(prev_val)
+        return curr_val
+    
+    def __next__(self):
+        return self.next()
+
+    def __str__(self):
+        return ''.join(self.front.reader()) + ' ' + ''.join(self.data.reader()) + ' ' + ''.join(self.back.reader())
+
+
 class KmpMatcher(object):
     def __init__(self, pattern, x, y):
         self.pattern = pattern.upper()
